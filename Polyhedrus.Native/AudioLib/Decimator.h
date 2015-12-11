f0d59dc2418f87133102ae2add1bae43a4a16e27
ValdemarOrn <valdiorn@gmail.com>
2015-12-11 23:21:20 +0000

    Improve OOX in decimator

--- Polyhedrus.Native/AudioLib/Decimator.h
+++ Polyhedrus.Native/AudioLib/Decimator.h
@@ -40,26 +40,39 @@ namespace AudioLib
 			values[17] = x0;
 			values[18] = x1;
 
-			float output =
-				Coeff[0] * values[0] +
-				Coeff[1] * values[1] +
-				Coeff[2] * values[2] +
-				Coeff[3] * values[3] +
-				Coeff[4] * values[4] +
-				Coeff[5] * values[5] +
-				Coeff[6] * values[6] +
-				Coeff[7] * values[7] +
-				Coeff[8] * values[8] +
-				Coeff[9] * values[9] +
-				Coeff[10] * values[10] +
-				Coeff[11] * values[11] +
-				Coeff[12] * values[12] +
-				Coeff[13] * values[13] +
-				Coeff[14] * values[14] +
-				Coeff[15] * values[15] +
-				Coeff[16] * values[16] +
-				Coeff[17] * values[17] +
-				Coeff[18] * values[18];
+			// This is faster than the vectorized version
+			float output = 
+				(
+					(
+						Coeff[0] * values[0] +
+						Coeff[1] * values[1] +
+						Coeff[2] * values[2] +
+						Coeff[3] * values[3] +
+						Coeff[4] * values[4]
+					) +
+					(
+						Coeff[5] * values[5] +
+						Coeff[6] * values[6] +
+						Coeff[7] * values[7] +
+						Coeff[8] * values[8]
+					)
+				) +
+				(
+					(
+						Coeff[9] * values[9] +
+						Coeff[10] * values[10] +
+						Coeff[11] * values[11] +
+						Coeff[12] * values[12] +
+						Coeff[13] * values[13]
+					) +
+					(
+						Coeff[14] * values[14] +
+						Coeff[15] * values[15] +
+						Coeff[16] * values[16] +
+						Coeff[17] * values[17] +
+						Coeff[18] * values[18]
+					)
+				);
 
 			return output;
 		}
